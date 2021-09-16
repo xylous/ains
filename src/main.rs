6d3f175c2ff53557ab34931ed7a5687c985e31da
xylous <xylous.e@gmail.com>
2021-09-16 22:11:23 +0300

    Add data structures for organising installation options

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,68 @@
 use clap::{App, Arg};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct InstallOptions {
+    username: Option<String>,
+    hostname: Option<String>,
+    #[serde(default = "default_editor")]
+    editor: String,
+    #[serde(default = "default_shell")]
+    shell: String,
+    #[serde(default = "default_bootloader")]
+    bootloader: String,
+    #[serde(default = "default_partitions")]
+    partitions: Vec<Partition>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Partition {
+    #[serde(default = "default_partition_format")]
+    format: String,
+    #[serde(default = "default_partition_disk")]
+    disk: String,
+    #[serde(default = "default_partition_size")]
+    size: String,
+    #[serde(default = "default_partition_mount")]
+    mount: String,
+}
+
+impl Partition {
+    fn new(format: String, disk: String, size: String, mount: String) -> Partition {
+        Partition {
+            format,
+            disk,
+            size,
+            mount,
+        }
+    }
+}
+
+fn default_editor() -> String { "vim".to_string() }
+fn default_shell() -> String { "bash".to_string() }
+fn default_bootloader() -> String { "grub".to_string() }
+fn default_partition_format() -> String { "ext4".to_string() }
+fn default_partition_disk() -> String { "/dev/nvme0n1".to_string() }
+fn default_partition_size() -> String { "".to_string() }
+fn default_partition_mount() -> String { "".to_string() }
+
+fn default_partitions() -> Vec<Partition> {
+    vec![
+        Partition::new(default_partition_format(),
+                        default_partition_disk(),
+                        default_partition_size(),
+                        default_partition_mount()),
+    ]
+}
+
+fn file_exists(path: &str) -> bool {
+    std::path::Path::new(path).exists()
+}
+
+fn file_to_string(path: &str) -> String
+{
+    std::fs::read_to_string(path).expect("failed to read file")
+}
 
 fn main() {
     let cli_args = App::new(env!("CARGO_PKG_NAME"))
@@ -10,5 +74,12 @@ fn main() {
             .about("input file"))
         .get_matches();
 
-    let input_file = cli_args.value_of("INPUT").unwrap();
+    let input_file = cli_args.value_of("FILE").unwrap();
+    if ! file_exists(input_file) {
+        eprintln!("error: file does not exist");
+        return;
+    }
+
+    let contents = file_to_string(input_file);
+    let _iopt: InstallOptions = serde_yaml::from_str(&contents).unwrap();
 }
