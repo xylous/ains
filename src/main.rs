48438034533c5e28a774c61b2c9f2eabc4310cce
xylous <xylous.e@gmail.com>
2021-09-16 21:53:43 +0300

    Use clap-rs to parse CLI args

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,14 @@
-fn main() {}
+use clap::{App, Arg};
+
+fn main() {
+    let cli_args = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("xylous, xylous.e@gmail.com")
+        .about("Install Arch from a config file")
+        .arg(Arg::new("FILE")
+            .required(true)
+            .about("input file"))
+        .get_matches();
+
+    let input_file = cli_args.value_of("INPUT").unwrap();
+}
