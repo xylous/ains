d3adbf97b0a6943de868ffccd85202e027cca46f
xylous <xylous.e@gmail.com>
2021-09-25 20:41:54 +0300

    Add function to create partitions on disk

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,9 +1,39 @@
 use std::process::{Command, Stdio};
 
+use crate::Partition;
+
+impl Partition {
+    pub fn create(&self) -> Result<(), std::io::Error> {
+        let echo_args = format!(
+            // n: create new partition
+            // next line: default partition number
+            // next line: default first sector
+            // use partition size specified in struct
+            "n\n\
+            \n\
+            \n\
+            {}\n",
+            &self.size,
+        );
+
+        let echo_cmd = Command::new("echo")
+            .arg("-n")
+            .args(echo_args.split("\n"))
+            .stdout(Stdio::piped())
+            .spawn()?;
+
+        // now, run fdisk
+        Command::new("fdisk")
+            .arg(format!("{}", self.disk))
+            .stdin(echo_cmd.stdout.unwrap())
+            .spawn()?;
+        Ok(())
+    }
+}
+
 pub fn install(packages: Vec<&str>) -> Result<(), std::io::Error> {
     let cmd: Vec<&str> = vec![vec!["pacman", "-S"], packages].into_iter().flatten().collect();
-    exec_cmd(&cmd)?;
-    Ok(())
+    exec_cmd(&cmd)
 }
 
 fn exec_cmd(args: &Vec<&str>) -> Result<(), std::io::Error> {
